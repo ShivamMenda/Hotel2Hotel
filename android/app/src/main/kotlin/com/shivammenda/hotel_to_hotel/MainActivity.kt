720ba6fc431773041993d14936c2979ce8f55024
Shivam Menda <ShivamMenda@users.noreply.github.com>
2021-02-19 17:35:22 +0530

    Adding All Files

---
+++ android/app/src/main/kotlin/com/shivammenda/hotel_to_hotel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shivammenda.hotel_to_hotel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_to_hotel/components/travelcard.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TravelApp(),
+  ));
+}
+
+class TravelApp extends StatefulWidget {
+  @override
+  _TravelAppState createState() => _TravelAppState();
+}
+
+class _TravelAppState extends State<TravelApp> {
+  List<String> urls = [
+    "https://resofrance.eu/wp-content/uploads/2018/09/hotel-luxe-mandarin-oriental-paris.jpg"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF6F7FF),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Color(0xFFF6F7FF),
+        title: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Welcome To H2H",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w600),
+            ),
+            Text(
+              "Pick Your Destination",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w300),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Material(
+              elevation: 10.0,
+              borderRadius: BorderRadius.circular(30.0),
+              shadowColor: Color(0x55434343),
+              child: TextField(
+                textAlign: TextAlign.start,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  hintText: "Search for a PG..",
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black54,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 30.0),
+            DefaultTabController(
+              length: 3,
+              child: Expanded(
+                child: Column(
+                  children: [
+                    TabBar(
+                      indicatorColor: Colors.deepOrange,
+                      unselectedLabelColor: Color(0xFF555555),
+                      labelColor: Colors.deepOrange,
+                      labelPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                      tabs: [
+                        Tab(
+                          text: "Trending",
+                        ),
+                        Tab(
+                          text: "Promotions",
+                        ),
+                        Tab(
+                          text: "Favorites",
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      height: 300.0,
+                      child: TabBarView(
+                        children: [
+                          Container(
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: [
+                                travelCard(
+                                    urls[0], "Rajeshwari PG", "Mysore", 3)
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: [
+                                travelCard(
+                                    urls[0], "Rajeshwari PG", "Mysuru", 3)
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: [],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Color(0xFFB7B7B7),
+        selectedItemColor: Color(0xFFFE8C68),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: "Bookmark",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: "Destination",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: "Notification",
+          ),
+        ],
+      ),
+    );
+  }
+}
